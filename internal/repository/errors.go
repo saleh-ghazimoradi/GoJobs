eecfc7475d4e18d4eb6c6a4a639c1459ddb090fe
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-11 14:19:35 +0330

    add some errors to the repository layer of the project

---
+++ internal/repository/errors.go
@@ -0,0 +1,9 @@
+package repository
+
+import "errors"
+
+var (
+	ErrRecordNotFound     = errors.New("record not found")
+	ErrDuplicateUsernames = errors.New("this username is already taken")
+	ErrDuplicateEmails    = errors.New("this email is already taken")
+)
