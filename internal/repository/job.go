fe024edbb6d0a02dd874f3738011e65e85f7bef8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-13 21:18:16 +0330

    implement get all jobs in the repository layer

--- internal/repository/job.go
+++ internal/repository/job.go
@@ -8,6 +8,7 @@ import (
 
 type Job interface {
 	CreateJob(ctx context.Context, job *service_models.Job) (*service_models.Job, error)
+	GetAllJobs(ctx context.Context) ([]*service_models.Job, error)
 	GetWithTXT(tx *sql.Tx) Job
 }
 
@@ -26,6 +27,27 @@ func (j *jobRepository) CreateJob(ctx context.Context, job *service_models.Job) 
 	}
 	job.ID = id
 	return job, nil
+}
+
+func (j *jobRepository) GetAllJobs(ctx context.Context) ([]*service_models.Job, error) {
+	query := `SELECT id, title, description, location, company, salary, created_at, user_id FROM jobs`
+	rows, err := j.dbRead.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var jobs []*service_models.Job
+	for rows.Next() {
+		var job service_models.Job
+		if err = rows.Scan(&job.ID, &job.Title, &job.Description, &job.Location, &job.Company, &job.Salary, &job.CreatedAt, &job.UserID); err != nil {
+			return nil, err
+		}
+		jobs = append(jobs, &job)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return jobs, nil
 }
 
 func (j *jobRepository) GetWithTXT(tx *sql.Tx) Job {
