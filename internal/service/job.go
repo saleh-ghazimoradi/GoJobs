f39e41f8d02fbf2faa4f4576f489c8416aacc95c
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-13 16:09:19 +0330

    implement job creation functionality in the service layer

---
+++ internal/service/job.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"github.com/saleh-ghazimoradi/GoJobs/internal/repository"
+	"github.com/saleh-ghazimoradi/GoJobs/internal/service/service_models"
+)
+
+type Job interface {
+	CreateJob(ctx context.Context, job *service_models.Job) (*service_models.Job, error)
+	GetWithTXT(tx *sql.Tx) Job
+}
+
+type jobService struct {
+	jobRepo repository.Job
+}
+
+func (j *jobService) CreateJob(ctx context.Context, job *service_models.Job) (*service_models.Job, error) {
+	return j.jobRepo.CreateJob(ctx, job)
+}
+
+func (j *jobService) GetWithTXT(tx *sql.Tx) Job {
+	return &jobService{
+		jobRepo: j.jobRepo.GetWithTXT(tx),
+	}
+}
+
+func NewJobService(jobRepo repository.Job) Job {
+	return &jobService{
+		jobRepo: jobRepo,
+	}
+}
