3f5e9543bf436a1b860ab1e96852741a05168d7a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-13 13:00:01 +0330

    implement userprofile picture update in the application layer

--- internal/gateway/user.go
+++ internal/gateway/user.go
@@ -5,9 +5,13 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/saleh-ghazimoradi/GoJobs/config"
 	"github.com/saleh-ghazimoradi/GoJobs/internal/service"
 	"github.com/saleh-ghazimoradi/GoJobs/internal/service/service_models"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -83,6 +87,73 @@ func (u *user) UpdateUserProfileHandler(w http.ResponseWriter, r *http.Request) 
 		}
 	}
 	if err = jsonResponse(w, http.StatusOK, updateUse); err != nil {
+		internalServerError(w, r, err)
+	}
+}
+
+func (u *user) UpdateUserProfilePictureHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	id, err := readIDParam(r)
+	if err != nil {
+		badRequestResponse(w, r, err)
+	}
+
+	userID, ok := r.Context().Value("userID").(int64)
+	if !ok {
+		unauthorizedErrorResponse(w, r, fmt.Errorf("unauthorized to update this user profile"))
+		return
+	}
+
+	isAdmin, ok := r.Context().Value("isAdmin").(bool)
+	if !ok {
+		unauthorizedErrorResponse(w, r, fmt.Errorf("unauthorized to update this user profile"))
+		return
+	}
+
+	if !isAdmin && userID != id {
+		unauthorizedErrorResponse(w, r, fmt.Errorf("unauthorized to update this user profile"))
+	}
+
+	err = r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		badRequestResponse(w, r, err)
+		return
+	}
+
+	file, header, err := r.FormFile("profile_picture")
+	if err != nil {
+		badRequestResponse(w, r, err)
+	}
+	defer file.Close()
+
+	uploadDir := config.AppConfig.UploadDIR.Upload
+	if err = os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		internalServerError(w, r, err)
+		return
+	}
+
+	filename := fmt.Sprintf("%d-%s", id, filepath.Base(header.Filename))
+	filePath := filepath.Join(uploadDir, filename)
+
+	saveFile, err := os.Create(filePath)
+	if err != nil {
+		internalServerError(w, r, err)
+		return
+	}
+	defer saveFile.Close()
+
+	if _, err := io.Copy(saveFile, file); err != nil {
+		internalServerError(w, r, err)
+		return
+	}
+
+	if err := u.userService.UpdateUserProfilePicture(ctx, id, filename); err != nil {
+		internalServerError(w, r, err)
+		return
+	}
+
+	if err := jsonResponse(w, http.StatusOK, "profile picture updated successfully"); err != nil {
 		internalServerError(w, r, err)
 	}
 }
