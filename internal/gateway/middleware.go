ab92cfaa1206edec3ed5b4481027e6e8b06a63e7
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-13 10:48:10 +0330

    implement the authentication middleware

---
+++ internal/gateway/middleware.go
@@ -0,0 +1,31 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"github.com/saleh-ghazimoradi/GoJobs/utils"
+	"net/http"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			unauthorizedErrorResponse(w, r, fmt.Errorf("authorization token missing"))
+			return
+		}
+
+		claims, err := utils.ValidateToken(token)
+
+		if err != nil {
+			unauthorizedErrorResponse(w, r, fmt.Errorf("authorization token missing"))
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "userID", claims.UserID)
+		ctx = context.WithValue(ctx, "isAdmin", claims.IsAdmin)
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+}
